f5ce44914147199a9c2f83f58f6aae4d8dd866f8
Steven Luong <s@MacBook-Pro-de-Steven.local>
2016-04-20 00:01:06 +0200

    player and rounds implemented

--- http/js/controllers.js
+++ http/js/controllers.js
@@ -5,6 +5,9 @@
 var rssReaderControllers = angular.module('rssReaderControllers', []);
 rssReaderControllers.controller('GoCtrl', ['$scope','Sources','News',
 		function($scope, Sources, News) {
+			$scope.game = {};
+			$scope.game.currentPlayer= "B";
+			$scope.game.round = 0;
 			$scope.locations = [];
 			for(var i=1;i<=19;i++){
 				for(var j=1;j<=19;j++){
@@ -16,43 +19,48 @@ rssReaderControllers.controller('GoCtrl', ['$scope','Sources','News',
 					$scope.locations.push(location);
 				}
 			}	
-			$scope.changeColor = function(location){
-				console.log(location);
-				//TODO change according to move color
+			$scope.play = function(location,game){
 				if(location.color=="E"){
-					location.color="W";
+					//TODO check if possible ? 
+					location.color=game.currentPlayer;
+					//TODO check if kill ?
+
+					//change player
+					game.round++;
+					if(game.currentPlayer=="B"){
+						game.currentPlayer="W";
+					}else{
+						game.currentPlayer="B";
+					}
 				}
-				else if(location.color=="W"){
-					location.color="B";
-				}
-				else if(location.color=="B"){
-					location.color="E";
+				else{
+					game.message = "location is not empty";
 				}
 
 			}
 			/*
-			Sources.getSources().success(function(response){
-				$scope.checkboxModel=[];
-				$scope.sources=[];
-				response.forEach(function(source){
-					$scope.checkboxModel[source.name]= source.display;
-					$scope.sources.push(source);
-				});
-			});
+			   Sources.getSources().success(function(response){
+			   $scope.checkboxModel=[];
+			   $scope.sources=[];
+			   response.forEach(function(source){
+			   $scope.checkboxModel[source.name]= source.display;
+			   $scope.sources.push(source);
+			   });
+			   });
 
-			$scope.news = [];
-			$scope.orderProp= "date";
-			News.getLastNews().success(function(response){
-				response.forEach(function(item){
-					var news = {};
-					news.date = item.date;
-					news.source= item.source;
-					news.img= item.image;
-					news.title= item.title;
-					news.link= item.title;
-					$scope.news.push(news);
-				})
-			});
-			*/
+			   $scope.news = [];
+			   $scope.orderProp= "date";
+			   News.getLastNews().success(function(response){
+			   response.forEach(function(item){
+			   var news = {};
+			   news.date = item.date;
+			   news.source= item.source;
+			   news.img= item.image;
+			   news.title= item.title;
+			   news.link= item.title;
+			   $scope.news.push(news);
+			   })
+			   });
+			   */
 		}]);
 
